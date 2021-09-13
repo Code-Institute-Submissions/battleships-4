820986ec38cc4e23089e0e09bd3b016112485237
cathalmc-dev <cathalmc.dev@gmail.com>
2021-09-13 21:02:25 +0000

    Add function to generate a random coordinate on the board

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from random import randint
+
 LETTERS = [" ", "A", "B", "C", "D", "E"]
 board = []
 
@@ -10,7 +12,19 @@ def print_board(board):
     for i in board:
         print(" ".join(i))
 
+
+def random_point(board):
+    return randint(1, len(board)-1)
+
+
+def random_coordinate(board):
+    return [random_point(board), random_point(board)]
+
+
+
 def main():
     print_board(board)
+    r = random_coordinate(board)
+    print(r)
 
 main()
