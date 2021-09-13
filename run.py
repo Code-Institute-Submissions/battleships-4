8bb0b67a80d8214e73f87e88d16cd63d336bcad6
cathalmc-dev <cathalmc.dev@gmail.com>
2021-09-13 22:43:46 +0000

    Add function which randomly generates a number of ship coordinates. The user can select how many ships to generate in the range of 3 to 6. Invalid input is handled.

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from random import randint
 
 LETTERS = [" ", "A", "B", "C", "D", "E"]
 board = []
+coordinates = []
 
 board.append(LETTERS)
 for i in range(0,5):
@@ -13,18 +14,43 @@ def print_board(board):
         print(" ".join(i))
 
 
-def random_point(board):
+def random_int(board):
     return randint(1, len(board)-1)
 
 
-def random_coordinate(board):
-    return [random_point(board), random_point(board)]
+def random_point():
+    point = int(str(random_int(board)) + str(random_int(board)))
+    return point
 
+
+def add_to_list():
+    r = random_point()
+    if (r in coordinates):
+        add_to_list()
+    else:
+        print(r)
+        coordinates.append(r)
+
+
+def hide_ships():
+    message = "How many ships would you like hidden? "
+    amount = input(message)
+    try:
+        amount = int(amount)
+    except ValueError:
+        message = "Please enter integer values only: "
+    else:
+        valid = 2 < amount < 7
+        if not valid:
+            message = "Enter an integer between 3 and 6: "
+    for i in range(0, amount):
+        add_to_list()
+    print(f"{amount} ships hidden. Good luck!")
 
 
 def main():
     print_board(board)
-    r = random_coordinate(board)
-    print(r)
+    hide_ships()
+    print(coordinates)
 
 main()
