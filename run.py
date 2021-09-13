091cea7d87d57f703833e6d5e04ff3c75409b6e5
cathalmc-dev <cathalmc.dev@gmail.com>
2021-09-13 20:51:28 +0000

    Add coordinates to board

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
+LETTERS = [" ", "A", "B", "C", "D", "E"]
 board = []
 
+board.append(LETTERS)
 for i in range(0,5):
-    board.append(["O"]*5)
+    board.append([f"{i + 1}"] + ["0"] * 5)
 
 
 def print_board(board):
     for i in board:
         print(" ".join(i))
 
-print_board(board)
+def main():
+    print_board(board)
+
+main()
