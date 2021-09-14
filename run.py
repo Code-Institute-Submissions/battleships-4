4d4ae524403d539093cba3e86f86b9982a3dd30f
cathalmc-dev <cathalmc.dev@gmail.com>
2021-09-14 03:01:25 +0000

    Add neccessary comments

--- run.py
+++ run.py
@@ -8,11 +8,17 @@ used = []
 
 
 def initialise():
+    """
+    Resets the board and used coordinate lists for a new round
+    """
     board.clear()
     used.clear()
 
 
 def create_board():
+    """
+    Creates the board at the start of a new round
+    """
     board.append(LETTERS)
     for i in range(0, 5):
         board.append([f"{i + 1}"] + ["O"] * 5)
@@ -28,11 +34,18 @@ def random_int(board):
 
 
 def random_point():
+    """
+    Generates 2 random numbers, concatenates them,
+    then converts the new number back to an integer for calculation
+    """
     point = int(str(random_int(board)) + str(random_int(board)))
     return point
 
 
 def add_to_list():
+    """
+    Generates new points and adds them to the coordinates list with no duplicates
+    """
     r = random_point()
     if (r in coordinates):
         add_to_list()
@@ -41,6 +54,10 @@ def add_to_list():
 
 
 def hide_ships():
+    """
+    Allows the player to choose how many battleships they want to find
+    Ensures valid input
+    """
     while True:
         amount = input("How many ships would you like hidden? ")
         try:
@@ -58,14 +75,27 @@ def hide_ships():
 
 
 def player_guess():
-    guess = str(input("Guess coordinate: "))
+    """
+    Gets the desired coordinate from the player,
+    coverts it into a 2 digit integer for calculation
+    and a point for representation on the visual board
+    """
+    guess = str(input("Guess coordinates (in the form A1): "))
+
+    # Checks that the players guess is legal
     check_length(guess)
     check_col(guess)
     check_row(guess)
+
+    # Conversion for calculation
     number = str(LETTERS.index(guess[0].capitalize())) + guess[1]
     number = int(number)
+
     check_used(number)
+
+    # Conversion for display
     shot = [LETTERS.index(guess[0].capitalize()), int(guess[1])]
+
     check_hit(number, shot)
 
 
@@ -81,11 +111,13 @@ def check_length(guess):
 
 
 def check_col(guess):
+    """
+    Takes the first character of the player's guess and compares it to the LETTERS list
+    """
     col = guess[0].capitalize()
     if (col in LETTERS):
         try:
             x = LETTERS.index(col)
-            return x
         except TypeError:
             print("Invalid coordinate")
     else:
@@ -128,18 +160,29 @@ def replay():
 
 
 def check_hit(number, shot):
+    """
+    Checks if a shot hits or not
+    """
     if (number in coordinates):
         print("Hit!")
         coordinates.remove(number)
+        print(f"{len(coordinates)} ships left.")
+
         used.append(number)
+
         board[shot[1]][shot[0]] = "X"
+
         check_win()
         print_board(board)
         player_guess()
     else:
         print("Miss!")
+        print(f"{len(coordinates)} ships left.")
+
         used.append(number)
+
         board[shot[1]][shot[0]] = "0"
+
         print_board(board)
         player_guess()
 
